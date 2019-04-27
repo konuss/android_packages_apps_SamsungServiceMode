dd50793af2fa3d8ed27e1b347226049ffa9ef12b
Dan Santos <dansantosbe@gmail.com>
2019-04-27 09:29:54 -0300

    SamsungServiceMode: Set LOCAL_PRIVATE_PLATFORM_APIS
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Change-Id: Iba3807f2d0c576e16388e590bbe47794631180e1

--- Android.mk
+++ Android.mk
@@ -9,6 +9,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := SamsungServiceMode
 LOCAL_JAVA_LIBRARIES := telephony-common
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
